1c92fd6af6b4341d08bfa43366f0f9f964eee255
Steven Bryant <stevelad_83@yahoo.com>
2023-03-08 12:29:34 -0500

    initial commit auth working with redirect to Home component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Auth from './components/Auth/Auth';
 import Header from './components/Header/Header';
+import Home from './components/Home.jsx';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Switch>
         <Route path="/auth/:type" component={Auth} />
+        <Route exact path="/home" component={Home} />
         <Route path="*">
           <Redirect to="/auth/sign-in" />
         </Route>
